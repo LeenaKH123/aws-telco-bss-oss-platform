6df234bee7ad83189101b53609557009b4a9b2c9
Leena <engleena@yahoo.com>
2026-06-03 09:07:32 +0300

    --

--- terraform/01-networking-vpc/provider.tf
+++ terraform/01-networking-vpc/provider.tf
@@ -1,12 +1,14 @@
+#defines terraform settings
 terraform {
+  #tells terraform which cloud provider plugins are needed
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source  = "hashicorp/aws" # download the AWS provider from HashiCorp, Terraform --> AWS API
+      version = "~> 5.0" # use version 5 of aws
     }
   }
 }
 
-provider "aws" {
-  region = var.aws_region
+provider "aws" { # configure AWS connection
+  region = var.aws_region # use region from variables
 }
